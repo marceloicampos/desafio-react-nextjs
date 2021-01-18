b2ea30f51bfca96669e7d994c729d6b0b252bf77
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-01-18 09:51:00 -0300

    update about

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,14 @@ function Sobre() {
             <Link href="/">
                 <a>Voltar a página PRINCIPAL</a>
             </Link>
-            </p> 
+            </p>
+            <hr />
+            <p>Meu e-Mail marceloicampos@outlook.com - <a href="mailto:marceloicampos@outlook.com"> mande um e-Mail pra mim</a> (eu garanto que respondo).</p>
+            <p><a href="https://github.com/marceloicampos" target="_blank"> /marceloicampos - Github</a> - Aqui você encontra meus trabalhos em desenvolvimento (com muitas dicas boas).</p>
+            <p><a href="https://twitter.com/marceloicampos" target="_blank"> /marceloicampos - Twitter</a> - É so mais um perfil básico, mas é onde busco muitas notícias e informações atualizadas.</p>
+            <p> <a href="https://www.facebook.com/marceloicampos" target="_blank"> /marceloicampos - Facebook</a> - Acompanha-me no meu perfil do Facebook.</p>
+            <p> <a href="https://www.instagram.com/marceloicampos" target="_blank"> /marceloicampos - Instagram</a> - Aqui tem algumas fotos minhas (nada demais).</p>
+            <p> <a href="https://www.youtube.com/MICChannel79" target="_blank"> /MICChannel79 - Youtube</a> - Acessa meu canal, não tem vídeo, mas tem perfil e MINHAS PLAYLISTS DE VÍDEOS (vale a pena CONFERIR).</p>
         </div>
     )
 }
