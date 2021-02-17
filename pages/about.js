4d654c390d74960a3aa22f5f43f67e199dc00709
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-02-16 22:14:42 -0300

    Project Update

--- pages/about.js
+++ pages/about.js
@@ -16,7 +16,7 @@ function About() {
             <h2>Meus Projetos</h2>
             <p><a href="https://marceloicampos.github.io/site-hello-world/" target="_blank">Projeto Hello World</a></p>
             <p><a href="https://marceloicampos.github.io/site-glass-html5/" target="_blank">Projeto Glass Html5</a></p>
-            <p><a href="https://marceloicampos.github.io/site-test-js/" target="_blank">Projeto Site Test JS</a></p>
+            <p><a href="https://marceloicampos.github.io/site-test-javascript/" target="_blank">Projeto Site Test JS</a></p>
             <h2>Como falar comigo?</h2>
             <p>Meu e-Mail marceloicampos@outlook.com - <a href="mailto:marceloicampos@outlook.com"> mande um e-Mail pra mim</a> (garanto que respondo).</p>
             <p><a href="https://github.com/marceloicampos" target="_blank"> /marceloicampos - GitHub</a> - Aqui você encontra meus trabalhos em desenvolvimento (com muitas dicas boas) e aqui meu <a href="https://marceloicampos.github.io" target="_blank">repositório público</a> no Git Hub.</p>
@@ -24,8 +24,7 @@ function About() {
             <p> <a href="https://www.facebook.com/marceloicampos" target="_blank"> /marceloicampos - Facebook</a> - Acompanha-me no meu perfil do Facebook.</p>
             <p> <a href="https://www.instagram.com/marceloicampos" target="_blank"> /marceloicampos - Instagram</a> - Aqui tem algumas fotos minhas (nada demais).</p>
             <p> <a href="https://www.youtube.com/user/MICChannel79/playlists" target="_blank"> /MICChannel79 - Youtube</a> - Acessa meu canal, não tem vídeo, mas tem perfil e MINHAS PLAYLISTS DE VÍDEOS (vale a pena CONFERIR).</p>
-            <script data-ad-client="ca-pub-8817498061942635" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            </div>
+        </div>
     )
 }
 
